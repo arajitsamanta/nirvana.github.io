38e3f11a2ff196af6d8409e6af07a39b996a9914
arijit24@gmail.com <arijit24@gmail.com>
2018-03-27 22:26:14 -0500

    inital commit

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp        = require('gulp');
+var bs          = require('browser-sync').create();   
+
+gulp.task('serve', [], () => {
+        bs.init({
+            server: {
+               baseDir: "./",
+            },
+            port: 5000,
+            reloadOnRestart: true            
+        });
+        gulp.watch('./**/*', ['', bs.reload]);
+});
